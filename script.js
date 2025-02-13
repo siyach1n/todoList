3136ef0668d09b242474dfb3eb2bb5ec8394b015
siyachin A Issc <siyachinoff@gmail.com>
2025-02-13 08:30:16 +0530

    re-work on code add state,component and render

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
-//updating code
-// ctr =0;
-// function counter(){
-//     documSent.querySelectorAll(".todo")[2].innerHTML="Counter: "+ctr;
-//     ctr=ctr+1;
-// }
-// setInterval(counter,1000)
+//optimize code
 
-//delete elements
+let todos=[];//state
 
-// function deleteTodo(index){
-//     const elements = document.getElementById("todo-"+index);
-//     const parentElement= document.getElementById("todoParent")
-//     parentElement.removeChild(elements)
-// }
+// component function 
+function addTodoComponent(todos,index){
+   const div1 =  document.createElement('div');
+   const h1=  document.createElement('h1');
+   const todoDelButton = document.createElement('button')
+   todoDelButton.innerHTML="Deletetodo"
+   
+   todoDelButton.setAttribute("onClick","deleteTodo(" + index+ ")");
 
-//adding elements
-
-// const divEL = document.createElement("div");
-// divEL.innerHTML="hi there"
-// const parentEl= document.querySelector("body");
-// parentEl.appendChild(divEL)
-
-let ctr = 1;
-function deleteTodo(index){
-    // console.log('delete todo called'); 
-    // const element =document.getElementById(index);
-    // element.parentNode.removeChild(element)
-
-    const elements = document.getElementById("todo-"+index);
-    elements.parentNode.removeChild(elements);
-    
+   h1.innerHTML=todos.title
+   div1.append(h1);
+   div1.append(todoDelButton    )
+   
+   
+   return div1;   
 }
+
+//function that modify state
 function addTodo(){
-     //getting input from the text box and named as inputValue
-    const value= document.querySelector("input").value;
-        //create span and button element
-    const spanEl = document.createElement("span");
-    const buttonEl = document.createElement("button")
-        //value to the elements 
-    spanEl.innerHTML= value;
-    buttonEl.innerHTML= "delete"
+    const input= document.querySelector("input");
+    const todotext = input.value.trim()
 
-    // buttonEl.onclick=function(){
-    //     deleteTodo('+ctr');
-    // }
-        //creating divel and append spanEl then buttonEl
-    const divEl=document.createElement("div");
-    divEl.setAttribute("id","todo-"+ctr)
-    divEl.appendChild(spanEl);
-    buttonEl.setAttribute("id","dl-btn")
-    divEl.appendChild(buttonEl);
-        //appending divEl to the body
-    document.querySelector(".todolist").appendChild(divEl)
+    if(todotext===""){
+        alert("todo is empty");
+        return;
+    }
+
+    todos.push({title:todotext});
+
+    render()
+}
+
+function delTodo(){
+    todos.splice(0,1)
+
+    render()
+}
+
+function deleteTodo(index){
+    todos.splice(index,1);
+    render();
+}
 
 
-    ctr++
-        // const inputValue=getValueInput.value;    
-
-    //     //creating a div element and add id to it 
-    // const newDivEL=document.createElement("div");
-    // newDivEL.setAttribute("id","todo-"+ctr)
-        
-     
-    // newDivEL.innerHTML="<div>"+inputValue +'</div> <button onclick="deleteTodo('+ ctr +')">delete</button>';
-    // const parentEl=document.querySelector(".todolist");
-    // parentEl.appendChild(newDivEL);
-    //   ctr = ctr +1
+//render   function
+function render(){
+        document.querySelector(".todoList").innerHTML="";
+    
+        for(let i=0;i< todos.length;i++){
+        const element = addTodoComponent(todos[i],i)
+        document.querySelector(".todoList").appendChild(element)
+    }
+    console.log(todos);
     
 }
